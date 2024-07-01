1234ec6c97ea8c00a38433e68dd882f9cf5253b1
Charity Njoroge <charity.njoroge@student.moringaschool.com>
2024-07-01 20:07:42 +0300

    update

--- server/app.py
+++ server/app.py
@@ -6,6 +6,8 @@ from flask_restful import Api, Resource
 
 from models import db, Plant
 
+
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -16,12 +18,40 @@ db.init_app(app)
 
 api = Api(app)
 
+
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        plant_list = []
+        for plant in plants:
+            plant_list.append(plant.to_dict())
+        return jsonify(plant_list)
+
+    def post(self):
+        json_data = request.get_json()
+        name = json_data.get('name')
+        image = json_data.get('image')  
+        price = json_data.get('price')
+
+        
+        if not name:
+            return make_response(jsonify({"error": "Please provide a name for the plant"}), 400)
+
+        new_plant = Plant(name=name, image=image, price=price)
+        db.session.add(new_plant)
+        db.session.commit()
+
+        return jsonify(new_plant.to_dict()), 201
+
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, plant_id):
+        plant = Plant.query.get_or_404(plant_id)
+        return jsonify(plant.to_dict())
+
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:plant_id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
